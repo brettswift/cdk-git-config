e85e57c2be32f894252bc55ab0880e642090cd12
Brett Swift <brettswift@gmail.com>
2019-12-03 13:24:57 -0700

    WIP - do not deploy account configs outside current account

--- lib/gitToSsm.ts
+++ lib/gitToSsm.ts
@@ -1,32 +1,50 @@
 import cdk = require('@aws-cdk/core');
 import ssm = require('@aws-cdk/aws-ssm')
-
 import configParser = require('./config/configuration')
+
+import STS = require('aws-sdk/clients/sts')
 
 export interface GitToSsmProps {
   configuration: configParser.ConfigLoader
+  /**
+   * Looks for a pattern in the root config of /account/<account_number. 
+   * Enabling this feature will ignore any configs outside of the current account.
+   */
+  onlyCurrentAccount?: boolean
 }
 
 export class GitToSsm extends cdk.Construct {
 
+  private onlyCurrentAccount: boolean
   constructor(scope: cdk.Construct, id: string, props: GitToSsmProps) {
     super(scope, id);
 
+    this.onlyCurrentAccount = props.onlyCurrentAccount || false
+    const sts = new STS()
+
+    const result = sts.getCallerIdentity().promise()
+    console.log(result)
+    
+    // const currentAccountId = sts
     const allConfigs = props.configuration.load()
 
     allConfigs.forEach(configGroup => {
-      
+      console.log(`${configGroup.configGroupName} ==? ${cdk.Stack.of(this).account}`)
+      if(configGroup.configGroupName === `account/curracount`){
+
+      }
       Object.keys(configGroup.configSets).forEach((key)=> {
 
         const value = configGroup.configSets[key]
 
-        new ssm.StringParameter(this, `${props.configuration.ssmRootPath}-${configGroup.configGroupName}-${key}`, {
+      
+        const param = new ssm.StringParameter(this, `${props.configuration.ssmRootPath}-${configGroup.configGroupName}-${key}`, {
           description: `from file: ${configGroup.relativePath}`,
           parameterName: key,
           type: ssm.ParameterType.STRING,
           stringValue: `${value}`,
         })
-
     });
   })
+
 }}
